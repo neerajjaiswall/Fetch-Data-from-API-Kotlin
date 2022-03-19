d5107ddb17d0bf03646e3cafd563e39533b64bca
neerajjaiswall <neerajjaiswal8952@gmail.com>
2022-03-19 13:52:36 +0530

    Initial Commit
    issues with calling api

---
+++ app/src/main/java/com/demo/countrylistretrofit/data/CountryModel.kt
@@ -0,0 +1,7 @@
+package com.demo.countrylistretrofit.data
+
+data class CountryModel(val name: String?,
+                    val status: String?,
+                    val species: String?,
+                    val gender: String?,
+                    val image: String?)
